d9e5f45c1438ad79233b28a9fd243ac0f53a28c4
Edison <edisonalberto21@hotmail.com>
2020-10-06 21:40:11 +0000

    Console

---
+++ aplicacion.js
@@ -0,0 +1,6 @@
+console.log('Prueba 1');
+console.warn('Warn 1');
+var edad = 7, puntos = 19;
+console.log(edad);
+var total = edad + puntos;
+console.log(total);
---
+++ aplicacion.ts
@@ -0,0 +1,12 @@
+console.log('Prueba 1')
+
+console.warn('Warn 1')
+
+var edad: number = 7,puntos:number = 19;
+console.log(edad)
+
+var total = edad + puntos
+
+console.log(total)
+
+
