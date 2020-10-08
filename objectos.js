9a98fdc3a3bb51940bcaace82e297a4e33c95ff4
Edison <edisonalberto21@hotmail.com>
2020-10-08 03:30:44 +0000

    Clases

--- objectos.js
+++ objectos.js
@@ -5,3 +5,25 @@ var persona = {
     peso: 37
 };
 console.log(persona.apellido);
+var array = ['uno', 'dos', 'tres'];
+console.log(array);
+var listado = [
+    {
+        nombre: 'edison',
+        apellido: 'Giraldo'
+    },
+    {
+        nombre: 'Alberto',
+        apellido: 'Restrepo'
+    }
+];
+for (var i = 0; i < array.length; i++) {
+    console.log(listado[i]);
+}
+for (var _i = 0, listado_1 = listado; _i < listado_1.length; _i++) {
+    var item = listado_1[_i];
+    console.log(item);
+}
+for (var item in listado) {
+    console.log(item);
+}
--- objectos.ts
+++ objectos.ts
@@ -7,3 +7,48 @@ var persona = {
 }
 
 console.log(persona.apellido)
+
+var array:Array<string> = ['uno','dos','tres']
+
+console.log(array);
+
+interface Usuario {
+    nombre: string;
+    apellido: string
+}
+
+var listado:Array<Usuario> = [
+    {
+        nombre:'edison',
+        apellido:'Giraldo'
+    },
+    {
+        nombre:'Alberto',
+        apellido:'Restrepo'
+    }
+]
+
+for(var i= 0;i<array.length;i++){
+    console.log(listado[i])
+}
+
+for(let item of listado){
+    console.log(item)
+}
+
+for(let item in listado){
+    console.log(item)
+}array
+
+
+class Alumno {
+    nombre:string;
+
+    mostrar(){
+        console.log("Hola,", this.nombre)
+    }
+}
+
+var alumno:Alumno = new Alumno();
+
+alumno.mostrar()
